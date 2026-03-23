fc903506abc2f1cffca0a512ff679d7be0853555
Asmod4n <791770+Asmod4n@users.noreply.github.com>
2026-03-23 19:39:28 +0100

    remove test code

--- test2.rb
+++
@@ -1,5 +0,0 @@
-nested_hash = {"hello" => {"foo" => {"bar" => [1,{"my" => "ass"},5]}}}
-CBOR.encode(nested_hash)
-lazy = CBOR.decode_lazy(CBOR.encode(nested_hash))
-puts lazy["hello"]["foo"]["bar"][1]["my"].value
-
